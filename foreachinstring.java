ca35e2f63b455b9719f25171f8394d97530bb8f3
I K TAYYIBA FATHIMA <tayyibafathima666@gmail.com>
2024-12-10 11:45:00 +0530

    Add files via upload

---
+++ foreachinstring.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class foreachinstring{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String a[]={"Apple","Orange","Pineapple"};
+        for(String b:a)
+        System.out.print(b+",");
+    }
+}        
